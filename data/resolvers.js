47fba5934b4e1f90ecbe782f101961ed5778a4c8
ShMcK <shawn.j.mckay@gmail.com>
2016-12-15 05:51:17 -0800

    refactor server code

--- data/resolvers.js
+++ data/resolvers.js
@@ -1,4 +1,6 @@
 import { pubsub } from './subscriptions'
+import Either from 'data.either'
+import { find, propEq } from 'ramda'
 
 const posts = [{
   id: 1, title: 'First Post', votes: 5,
@@ -10,26 +12,29 @@ const posts = [{
 
 const resolveFunctions = {
   Query: {
-    posts() {
-      return posts
-    },
+    posts: () => posts,
   },
   Mutation: {
-    upvotePost(_, { postId }) {
-      const post = posts.find(p => p.id === postId)
-      if (!post) {
-        throw new Error(`Couldn't find post with id ${postId}`)
-      }
-      post.votes += 1
-      // update on post vote
-      pubsub.publish('postUpvoted', post)
-      return post
+    upvotePost: (_, { postId }) => {
+      return Either.of(posts)
+        .map(find(propEq('id', postId)))
+        .map(post => {
+          post.votes += 1
+          return post
+        })
+        .fold(
+        e => {
+          throw new Error(`Couldn't find post with id ${postId}`)
+        },
+        post => {
+          pubsub.publish('postUpvoted', post)
+          return post
+        }
+        )
     },
   },
   Subscription: {
-    postUpvoted(post) {
-      return post
-    },
+    postUpvoted: (post) => post,
   },
 }
 
--- server/graphqlServer.js
+++ server/graphqlServer.js
@@ -9,23 +9,21 @@ const createGraphQLServer = (port) => {
 
   const server = express().use('*', cors())
 
-  server.use('/graphql', bodyParser.json(), graphqlExpress({
-    schema,
-    context: {},
-  }))
-
-  server.use('/graphiql', graphiqlExpress({
-    endpointURL: '/graphql',
-  }))
-
-  server.use('/schema', (req, res) => {
-    res.set('Content-Type', 'text/plain')
-    res.send(printSchema(schema))
-  })
-
-  server.listen(port, () => {
-    console.log(`GraphQL Server is now running on http://localhost:${port}/graphql`)
-  })
+  server
+    .use('/graphql', bodyParser.json(), graphqlExpress({
+      schema,
+      context: {},
+    }))
+    .use('/graphiql', graphiqlExpress({
+      endpointURL: '/graphql',
+    }))
+    .use('/schema', (req, res) => {
+      res.set('Content-Type', 'text/plain')
+      res.send(printSchema(schema))
+    })
+    .listen(port, () => {
+      console.log(`GraphQL Server is now running on http://localhost:${port}/graphql`)
+    })
 
 }
 
